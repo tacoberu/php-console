779ad3a475b66507177b1a18da000842d8046404
Martin Takáč <martin@takac.name>
2018-01-20 01:21:08 +0100

    sample

--- samples/phase-4/app.php
+++ samples/phase-4/app.php
@@ -11,6 +11,15 @@ if (@!include __DIR__ . '/vendor/autoload.php') {
 require __dir__ . '/vendor/autoload.php';
 
 $app = Taco\Console\SingleShell::create(
+	/**
+	 * Lorem ipsum doler ist.
+	 *
+	 * @author John Dee <john@dee.tld>
+	 *
+	 * @argument string $src Source file
+	 * @argument string $desc Destination
+	 * @optional int $limit How many characters to copy
+	 */
 	function($src, $desc, $limit = 10) {
 		file_put_contents($desc, substr(file_get_contents($src), 0, $limit));
 		return 0;
